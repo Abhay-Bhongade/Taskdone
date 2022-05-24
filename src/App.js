158ef5201e808bcabb816fb4671a7f4989f74b38
Abhay Bhongade <abhaybhongade63@gmail.com>
2022-05-24 11:42:09 +0530

    Add data

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddData from './AddData'
+import Todo from './Todo'
+
+const App = () => {
+  return (
+    <>
+      <AddData />
+    </>
+  )
+}
+
+export default App
